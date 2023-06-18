33995dbe4cbe251c9fb49d2fb05f07d8ba86cd45
Hemant Singh <111867265+HemantSingh23@users.noreply.github.com>
2023-06-18 18:11:05 +0530

    Time: 3 ms (92.43%), Space: 8.9 MB (48.77%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,94 @@
+
+
+
+
+// //
+// // // // // // //  Solve Using  Only One Method  ===>     
+// // // // // // //  Solve Using  Only One Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method  ====>  Simple Iteration (Two Pointer)    TC = O(n) , SC = O(1) constant
+// //                      
+// //                      
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// class Solution {
+// public:
+//     vector<int> diStringMatch(string s) {
+        
+//     }
+// };
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method    ======>    Simple Iteration (Two Pointer) 
+// // //  1st Method    ======>    Simple Iteration (Two Pointer) 
+// // //  1st Method    ======>    Simple Iteration (Two Pointer) 
+// //                             
+// // //   Time Complexity   = O(n)   
+// //                              
+// // //   Space Complexity  = O(1) constant  
+// //                               
+
+
+
+
+class Solution {
+public:
+    vector<int> diStringMatch(string s) 
+    {
+        int sz = s.size() ;
+        vector<int> perm;
+        int high = sz ;
+        int low = 0 ;
+        for(int i=0; i<sz; i++)
+        {
+            if(s[i]=='I') perm.push_back(low) , low ++ ;
+            else perm.push_back(high) , high -- ;
+        }
+        if(s[sz-1]=='I') perm.push_back(low) , low ++ ;
+        else perm.push_back(high) , high -- ;
+        return perm ;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
