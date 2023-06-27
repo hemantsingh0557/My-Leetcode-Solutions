de4fd94c7fe2df6e5090a357520b58643dec1baa
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-06-27 13:30:40 +0530

    Time: 64 ms (94.78%), Space: 13.1 MB (86.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,11 +1,27 @@
 
 
 
+
+
+
+
+// //
+// // // // // // //  Solve Using One Method  ===>     
+// // // // // // //  Solve Using One Method  ===>     
+// // // // // // //  Solve Using One Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method  ====>  Dp Memoization   TC = O(amount * n) , SC = O(amount) 
+// //                      
+// //                      
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 // class Solution {
 // public:
-//     // // //  Main Function of Question ==>     Main Function of Question ==>
-//     // // //  Main Function of Question ==>     Main Function of Question ==>
-//     //
 //     int coinChange(vector<int>& coins, int amount) 
 //     {
 //     }
@@ -13,38 +29,55 @@
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method    ======>    Dp Memoization  
+// // //  1st Method    ======>    Dp Memoization  
+// // //  1st Method    ======>    Dp Memoization  
+// //                             
+// // //   Time Complexity   = O(amount * n) 
+// //                              
+// // //   Space Complexity  = O(amount)   
+// //                               
 
 
 
 
 class Solution {
 public:
-
     int dp[10010] ;
-    int UsedCoin(vector<int>& coins, int amount)
+    int func(int amount, vector<int> &coins)
     {
-        if(amount==0) return 0;
-        if(dp[amount] != -1 ) return dp[amount]  ;
+        if(amount ==0 ) return 0;
+        if(dp[amount] != -1 ) return dp[amount];
         int ans = INT_MAX ;
-        for( int coin : coins )
+        for(int coin : coins)
         {
-            if(amount-coin>=0)
-            ans = min( ans+0LL , UsedCoin(coins , amount-coin )+1LL ) ; 
+            if(amount - coin >= 0 ) 
+            ans = min (ans + 0LL , func(amount-coin, coins) +1LL) ;
         }
-        return dp[amount]  = ans  ;
+        return dp[amount] = ans ;
     }
-
     // // //  Main Function of Question ==>     Main Function of Question ==>
     // // //  Main Function of Question ==>     Main Function of Question ==>
     //
     int coinChange(vector<int>& coins, int amount) 
     {
         memset(dp, -1, sizeof(dp)) ;
-        int ans = UsedCoin(coins , amount) ;
-        return ans==INT_MAX ? -1 : ans ;
+        int ans = func(amount , coins) ;
+        return ans == INT_MAX ? -1 : ans ;
     }
 };
 
@@ -52,40 +85,6 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int dp[10010] ;
-//     int func(int amount, vector<int> &coins)
-//     {
-//         if(amount ==0 ) return 0;
-//         if(dp[amount] != -1 ) return dp[amount];
-//         int ans = INT_MAX ;
-//         for(int coin : coins)
-//         {
-//             if(amount - coin >= 0 ) 
-//             ans = min (ans + 0LL , func(amount-coin, coins) +1LL) ;
-//         }
-//         return dp[amount] = ans ;
-//     }
-//     // // //  Main Function of Question ==>     Main Function of Question ==>
-//     // // //  Main Function of Question ==>     Main Function of Question ==>
-//     //
-//     int coinChange(vector<int>& coins, int amount) 
-//     {
-//         memset(dp, -1, sizeof(dp)) ;
-//         int ans = func(amount , coins) ;
-//         return ans == INT_MAX ? -1 : ans ;
-//     }
-// };
 
 
 
