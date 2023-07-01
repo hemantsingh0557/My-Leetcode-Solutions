9095a183767a7e438379754a66d376e74afde2c4
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-07-01 18:35:36 +0530

    Time: 1550 ms (53.97%), Space: 6.8 MB (62.54%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,100 @@
+
+
+
+
+
+
+
+// //
+// // // // // // //  Solve Using  Only One Method  ===>     
+// // // // // // //  Solve Using  Only One Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method  ====>  Simple Recursion and Backtracking    TC = O(k^n) , SC = O(k) 
+// //                      
+// //                      
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// class Solution {
+// public:
+//     int distributeCookies(vector<int>& cookies, int k) 
+//     {
+        
+//     }
+// };
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method    ======>     Simple Recursion and Backtracking 
+// // //  1st Method    ======>     Simple Recursion and Backtracking 
+// // //  1st Method    ======>     Simple Recursion and Backtracking 
+// //                             
+// // //   Time Complexity   = O(k^n)   
+// //                              
+// // //   Space Complexity  = O(k)   
+// //                              
+
+
+
+class Solution {
+public:
+    int n ;
+    int ans = INT_MAX ;
+    void minimumCookies(int i , int k , vector<int>& child , vector<int>& cookies)
+    {
+        if(i>=n)
+        {
+            ans = min( ans , *max_element(child.begin(), child.end()) ) ;   
+            return ;
+        }
+        for(int j=0; j<k; j++)
+        {
+            child[j] += cookies[i] ; 
+            minimumCookies(i+1, k ,child , cookies ) ;   
+            child[j] -= cookies[i] ;
+        }
+    }
+    // // // // //    Main Function of Question    ==========>
+    // // // // //    Main Function of Question    ==========>
+    //
+    int distributeCookies(vector<int>& cookies, int k) 
+    {
+        n = cookies.size() ;
+        vector<int> child(k,0) ;
+        minimumCookies(0,k ,child , cookies ) ;
+        return ans ;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
