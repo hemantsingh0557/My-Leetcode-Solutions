7633ee94e45e9bc48fa05223cba356184f660208
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-07-02 21:21:58 +0530

    Time: 142 ms (40.83%), Space: 50 MB (5.34%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -3,19 +3,20 @@
 
 
 
-
-
 // //
-// // // // // // //  Solve Using  Two  Method  ===>     
-// // // // // // //  Solve Using  Two  Method  ===>                        
+// // // // // // //  Solve Using  Three  Method  ===>     
+// // // // // // //  Solve Using  Three  Method  ===>                        
 // //                     
 // //                     
-// // //  1st Method  ====>   Using sort() function    TC = O(nlog(n)) , SC = O(1) constant
+// // //  1st Method  ====>   Using sort() function      TC = O(nlog(n)) , SC = O(1) constant
 // //                      
 // //                      
-// // //  2nd Method  ====>     TC = O(n) , SC = O(1) constant
+// // //  2nd Method  ====>   Min Heap    TC = O(nlog(n))   , SC = O(1) constant
 // //                     
 // //                     
+// // //  3rd Method  ====>   Max Heap    TC = O(nlog(n))   , SC = O(1) constant
+// //
+// //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,15 +57,15 @@
 // //                               
 
 
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) 
-    {
-        int n = nums.size() ;
-        sort(nums.begin() , nums.end()) ;
-        return nums[n-k] ;
-    }
-};
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) 
+//     {
+//         int n = nums.size() ;
+//         sort(nums.begin() , nums.end()) ;
+//         return nums[n-k] ;
+//     }
+// };
 
 
 
@@ -76,16 +77,14 @@ public:
 
 // //                           
 // //                           
-// // //  2nd Method    ======> 
-// // //  2nd Method    ======> 
-// // //  2nd Method    ======> 
+// // //  2nd Method    ======>    Min Heap 
+// // //  2nd Method    ======>    Min Heap 
+// // //  2nd Method    ======>    Min Heap 
 // //                             
-// // //   Time Complexity   = O(n)   
+// // //   Time Complexity   = O(nlog(n))   
 // //                              
 // // //   Space Complexity  = O(1) constant  
 // //                               
-
-
 
 
 
@@ -93,9 +92,60 @@ public:
 // public:
 //     int findKthLargest(vector<int>& nums, int k)
 //     {
-        
+//         int n= nums.size() ;
+//         priority_queue<int , vector<int> , greater<int>> minHeap ;
+//         for(int i=0; i<k; i++)
+//         {
+//             minHeap.push(nums[i]) ;
+//         }
+//         for(int i=k; i<n; i++)
+//         {
+//             if(nums[i]>minHeap.top())
+//             {
+//                 minHeap.pop() ;
+//                 minHeap.push(nums[i]) ;
+//             }
+//         }
+//         return  minHeap.top() ; 
 //     }
 // };
+
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  3nd Method    ======>    Max Heap 
+// // //  3nd Method    ======>    Max Heap 
+// // //  3nd Method    ======>    Max Heap 
+// //                             
+// // //   Time Complexity   = O(nlog(n))   
+// //                              
+// // //   Space Complexity  = O(1) constant  
+// //                               
+
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        int n= nums.size() ;
+        priority_queue<int > pq ;
+        for(int i=0; i<n; i++)
+        {
+            pq.push(nums[i]) ;
+        }
+        for(int i=1; i<k; i++)
+        {
+            pq.pop() ;
+        }
+        return  pq.top() ; 
+    }
+};
 
 
 
