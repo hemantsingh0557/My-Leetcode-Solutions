f5d64559b5657ccceaa4433b7a84ca66e18a22d8
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-07-25 19:15:20 +0530

    Time: 218 ms (9.90%), Space: 19.7 MB (42.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,29 +5,6 @@
 
 
 
-// //
-// // // // // // //  Solve Using Three Method  ===>     
-// // // // // // //  Solve Using Three Method  ===>                        
-// //                 
-// //    
-// // // don't take it or take it and if take it then again don't take it or take it
-// // // and this will continue untill target will be 0 or less than zero here k is average length
-// // // and t is average no of time a particular element can be  don't take it or take  it
-// //
-// // //  1st Method  ====>  Simple Recursion(TLE TLE TLE)   TC = O((2^t)*k) , SC = O(1) 
-// //
-// //
-// // //  2nd Method  ====>  Simple Recursion(TLE TLE TLE)   TC = O(amount*(2^n)) , SC = O(1) 
-// //                      
-// //
-// // //  3rd Method  ====>  Dp Memoization   TC = O(amount * n) , SC = O(amount) 
-// //             
-// //         
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 // class Solution {
 // public:
@@ -38,6 +15,34 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// // // // // // //  Solve Using Two Method  ===>     
+// // // // // // //  Solve Using Two Method  ===>                        
+// //                 
+// //    
+// // // don't take it or take it and if take it then again don't take it or take it
+// // // and this will continue untill target will be 0 or less than zero here k is average length
+// // // and t is average no of time a particular element can be  don't take it or take  it
+// //
+// // 1st Method (Three Ways) ==> Simple Recursion(TLE TLE TLE)  TC = O(amount * (2^n) , SC = O(1) 
+// //
+// //
+// // 2nd Method (Two ways) ==>  Dp Memoization    TC = O(amount * n ) , SC = O(amount) 
+// //                      
+// //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -48,18 +53,22 @@
 
 
 
+
+                          
 // //                           
-// //                           
 // // //  1st Method    ======>    Simple Recursion(TLE TLE TLE) 
 // // //  1st Method    ======>    Simple Recursion(TLE TLE TLE) 
 // // //  1st Method    ======>    Simple Recursion(TLE TLE TLE) 
-// //                             
-// // //   Time Complexity   = O((2^t)  * k ) (Probably)
+// //                                                    
+// // //   Time Complexity   = O(amount * (2^n))  
 // //                              
 // // //   Space Complexity  = O(1)   
-// //                               
+// //                              
 
 
+// // //   1st  Method   ====>      1st way   =====>       
+// // //   1st  Method   ====>      1st way   =====>       
+// // //   1st  Method   ====>      1st way   =====>       
 
 // class Solution {
 // public:
@@ -91,20 +100,41 @@
 
 
 
+// // //   1st  Method   ====>      2nd  way   =====>       
+// // //   1st  Method   ====>      2nd  way   =====>       
+// // //   1st  Method   ====>      2nd  way   =====>        
+
+// class Solution {
+// public:
+//     int n ; 
+//     int func(int i , int amount, vector<int> &coins)
+//     {
+//         if(i==n && amount !=0) return INT_MAX ;
+//         if(amount==0) return 0 ;
+//         int ans = func(i+1 , amount , coins) ;
+//         if(amount - coins[i] >= 0 ) 
+//         ans = min(ans+0LL , 1LL+ func(i , amount-coins[i] , coins) ) ;
+//         return ans ;
+//     }
+//     // // //  Main Function of Question ==>     Main Function of Question ==>
+//     // // //  Main Function of Question ==>     Main Function of Question ==>
+//     //
+//     int coinChange(vector<int>& coins, int amount) 
+//     {
+//         n = coins.size() ; 
+//         int ans = func(0, amount , coins) ;
+//         return ans == INT_MAX ? -1 : ans ;
+//     }
+// };
 
 
-// //                           
-// //                           
-// // //  2nd  Method    ======>   Simple Recursion(TLE TLE TLE)  
-// // //  2nd  Method    ======>   Simple Recursion(TLE TLE TLE)  
-// // //  2nd  Method    ======>   Simple Recursion(TLE TLE TLE)  
-// //                             
-// // //   Time Complexity   = O(amount * (2^n))   (Probably)
-// //                              
-// // //   Space Complexity  = O(1)   
-// //                               
 
 
+
+// // //   1st  Method   ====>    3rd  way   =====>       
+// // //   1st  Method   ====>    3rd  way   =====>       
+// // //   1st  Method   ====>    3rd  way   =====>       
+                              
 // class Solution {
 // public:
 //     int func(int amount, vector<int> &coins)
@@ -134,11 +164,20 @@
 
 
 
+
+
+
+
+
+
+
+
 // //                           
 // //                           
-// // //  3rd  Method    ======>    Dp Memoization  
-// // //  3rd  Method    ======>    Dp Memoization  
-// // //  3rd  Method    ======>    Dp Memoization  
+// // //  2nd  Method ( Two ways )   ======>    Dp Memoization  
+// // //  2nd  Method ( Two ways )   ======>    Dp Memoization  
+// // //  2nd  Method ( Two ways )   ======>    Dp Memoization  
+// //
 // //                             
 // // //   Time Complexity   = O(amount * n) 
 // //                              
@@ -146,31 +185,105 @@
 // //                               
 
 
+
+// // //   2nd Method   ====>      1st way   =====>       
+// // //   2nd Method   ====>      1st way   =====>       
+// // //   2nd Method   ====>      1st way   =====>       
+
+
+// class Solution {
+// public:
+//     int dp[10010] ;
+//     int func(int amount, vector<int> &coins)
+//     {
+//         if(amount ==0 ) return 0;
+//         if(dp[amount] != -1 ) return dp[amount];
+//         int ans = INT_MAX ;
+//         for(int coin : coins)
+//         {
+//             if(amount - coin >= 0 ) 
+//             ans = min (ans + 0LL , func(amount-coin, coins) +1LL) ;
+//         }
+//         return dp[amount] = ans ;
+//     }
+//     // // //  Main Function of Question ==>     Main Function of Question ==>
+//     // // //  Main Function of Question ==>     Main Function of Question ==>
+//     //
+//     int coinChange(vector<int>& coins, int amount) 
+//     {
+//         memset(dp, -1, sizeof(dp)) ;
+//         int ans = func(amount , coins) ;
+//         return ans == INT_MAX ? -1 : ans ;
+//     }
+// };
+
+
+
+
+
+
+// // //   2nd Method   ====>      2nd  way   =====>       
+// // //   2nd Method   ====>      2nd  way   =====>       
+// // //   2nd Method   ====>      2nd  way   =====>       
+
+
 class Solution {
 public:
-    int dp[10010] ;
-    int func(int amount, vector<int> &coins)
+    int n ; 
+    int dp[20][100000] ;
+    int func(int i , int amount, vector<int> &coins)
     {
-        if(amount ==0 ) return 0;
-        if(dp[amount] != -1 ) return dp[amount];
-        int ans = INT_MAX ;
-        for(int coin : coins)
-        {
-            if(amount - coin >= 0 ) 
-            ans = min (ans + 0LL , func(amount-coin, coins) +1LL) ;
-        }
-        return dp[amount] = ans ;
+        if(i==n && amount !=0) return INT_MAX ;
+        if(amount==0) return 0 ;
+        if(dp[i][amount] != -1) return dp[i][amount] ;
+        int ans = func(i+1 , amount , coins) ;
+        if(amount - coins[i] >= 0 ) 
+        ans = min(ans+0LL , 1LL+ func(i , amount-coins[i] , coins) ) ;
+        return dp[i][amount] =  ans ;
     }
     // // //  Main Function of Question ==>     Main Function of Question ==>
     // // //  Main Function of Question ==>     Main Function of Question ==>
     //
     int coinChange(vector<int>& coins, int amount) 
     {
-        memset(dp, -1, sizeof(dp)) ;
-        int ans = func(amount , coins) ;
+        memset(dp , -1 , sizeof(dp)) ;
+        n = coins.size() ; 
+        int ans = func(0, amount , coins) ;
         return ans == INT_MAX ? -1 : ans ;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
