422851f9594c7a7c0bbe3f637d18a4f357bc6d1f
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-08-04 10:13:29 +0530

    Time: 8 ms (68.35%), Space: 9.2 MB (67.47%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -26,6 +26,15 @@
 
 
 
+
+
+
+
+
+
+
+
+
 // //
 // // // // // // //  Solve Using  Two  Method  ===>     
 // // // // // // //  Solve Using  Two  Method  ===>           
@@ -33,15 +42,19 @@
 // //                     
 // //                 // //  n is size of s and m is size of wrodDist             
 // //                     
-// // //  1st Method  ====> Simple Recursion (TLE TLE TLE)    TC = O() , SC = O(m) 
+// // //  1st Method (Two ways)  ====> Simple Recursion (TLE TLE TLE)    TC = O() , SC = O(m) 
 // //                      
 // //                      
-// // //  2nd Method  ====> Dp Memoization    TC = O(m + n^2 ) or O(m * n^2 ) , SC = O(m + n^2 )
+// // //  2nd Method (Two ways)   ====> Dp Memoization    TC = O(m + n^2 ) or O(m * n^2 ) , SC = O(m + n^2 )
+// //                                                  // //  for more details see below 
 // //                     
-// //                     
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 
 
@@ -49,15 +62,22 @@
 
 // //                           
 // //                           
-// // //  1st Method    ======>     Simple Recursion (TLE TLE TLE)  
-// // //  1st Method    ======>     Simple Recursion (TLE TLE TLE)  
-// // //  1st Method    ======>     Simple Recursion (TLE TLE TLE)  
+// // //  1st Method  (Two ways)    ======>     Simple Recursion (TLE TLE TLE)  
+// // //  1st Method  (Two ways)    ======>     Simple Recursion (TLE TLE TLE)  
+// // //  1st Method  (Two ways)    ======>     Simple Recursion (TLE TLE TLE)  
 // //                             
 // // //   Time Complexity   = O()   
 // //                              
 // // //   Space Complexity  = O(m)   
 // //                               
 
+
+
+
+
+// // //  1st  Method   ====>     1st  way   =====>       
+// // //  1st  Method   ====>     1st  way   =====>       
+// // //  1st  Method   ====>     1st  way   =====>    
 
 
 // class Solution {
@@ -100,6 +120,48 @@
 
 
 
+// // //  1st  Method   ====>     2nd  way   =====>       
+// // //  1st  Method   ====>     2nd  way   =====>       
+// // //  1st  Method   ====>     2nd  way   =====>       
+
+
+// class Solution {
+// public:
+//     int n , m   ; 
+//     bool check(int i , string s, vector<string>& wordDict )
+//     {
+//         if(i==n) return true ;
+//         string temp ;
+//         for(int j=i; j<n; j++)
+//         {
+//             temp.push_back( s[j] ) ;
+//             for(int k=0; k<m; k++ )
+//             {
+//                 if(wordDict[k]==temp) if( check( j+1 , s , wordDict ) ) return true ;  
+//             }
+//         }
+//         return false ;
+//     }
+//     // // // // //    Main Function of Question    ==========>
+//     // // // // //    Main Function of Question    ==========>
+//     //
+//     bool wordBreak(string s, vector<string>& wordDict) 
+//     {
+//         n = s.size() , m = wordDict.size() ;
+//         return check( 0 , s , wordDict ) ;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -111,9 +173,9 @@
 
 // //                           
 // //                           
-// // //  2nd Method    ======>    Dp Memoization  
-// // //  2nd Method    ======>    Dp Memoization  
-// // //  2nd Method    ======>    Dp Memoization  
+// // //  2nd Method  (Two ways)    ======>    Dp Memoization  
+// // //  2nd Method  (Two ways)    ======>    Dp Memoization  
+// // //  2nd Method  (Two ways)    ======>    Dp Memoization  
 // //                             
 // // //   Time Complexity   = O(m + n^2 )    if set.count() in Best case
 // // //   Time Complexity   = O(m * n^2 )    if set.count() in Worst case
@@ -124,32 +186,83 @@
 
 
 
+// // //   2nd Method   ====>      1st way   =====>       
+// // //   2nd Method   ====>      1st way   =====>       
+// // //   2nd Method   ====>      1st way   =====>       
+
+
+// class Solution {
+// public:
+//     int n ;
+//     int dp[350] ;
+//     unordered_set<string> st ;
+//     bool isPossible(int i , string s, vector<string>& wordDict )
+//     {
+//         if(i==n) return true ;
+//         bool ans = false ;
+//         if(dp[i] != -1) return dp[i] ;
+//         for(int j = i ; j<n; j++)
+//         {
+//             string temp = s.substr(i, j-i+1 ) ;
+//             //
+//             //
+//             // if( ! st.count(temp)  ) continue ;   
+//             // bool check = isPossible( j+1 , s , wordDict) ;
+//             // if( check )  
+//             //
+//             if( st.count(temp) && isPossible( j+1 , s , wordDict) ) 
+//             {
+//                 ans = true ;   
+//                 break ;
+//             }
+//         }
+//         return dp[i] =  ans ;
+//     }
+//     // // // // //    Main Function of Question    ==========>
+//     // // // // //    Main Function of Question    ==========>
+//     //
+//     bool wordBreak(string s, vector<string>& wordDict) 
+//     {
+//         memset(dp , -1 , sizeof(dp)) ;
+//         n = s.size() ;
+//         int m = wordDict.size() ;
+//         for(int i=0; i<m; i++) st.insert(wordDict[i]) ;
+//         int ans = isPossible( 0 , s , wordDict ) ;
+//         return ans ;  
+//     }
+// };
+
+
+
+
+
+
+
+
+// // //   2nd Method   ====>     2nd  way   =====>       
+// // //   2nd Method   ====>     2nd  way   =====>       
+// // //   2nd Method   ====>     2nd  way   =====>       
+
+
+
 class Solution {
 public:
-    int n ;
-    int dp[350] ;
-    unordered_set<string> st ;
-    bool isPossible(int i , string s, vector<string>& wordDict )
+    int n , m   ; 
+    int dp[310] ; 
+    bool check(int i , string s, vector<string>& wordDict )
     {
         if(i==n) return true ;
-        bool ans = false ;
-        if(dp[i] != -1) return dp[i] ;
-        for(int j = i ; j<n; j++)
+        if( dp[i] != -1 ) return dp[i] ;
+        string temp ;
+        for(int j=i; j<n; j++)
         {
-            string temp = s.substr(i, j-i+1 ) ;
-            //
-            //
-            // if( ! st.count(temp)  ) continue ;   
-            // bool check = isPossible( j+1 , s , wordDict) ;
-            // if( check )  
-            //
-            if( st.count(temp) && isPossible( j+1 , s , wordDict) ) 
+            temp.push_back( s[j] ) ;
+            for(int k=0; k<m; k++ )
             {
-                ans = true ;   
-                break ;
+                if(wordDict[k]==temp) if( check( j+1 , s , wordDict ) ) return true ;  
             }
         }
-        return dp[i] =  ans ;
+        return dp[i] = false ;
     }
     // // // // //    Main Function of Question    ==========>
     // // // // //    Main Function of Question    ==========>
@@ -157,20 +270,10 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) 
     {
         memset(dp , -1 , sizeof(dp)) ;
-        n = s.size() ;
-        int m = wordDict.size() ;
-        for(int i=0; i<m; i++) st.insert(wordDict[i]) ;
-        int ans = isPossible( 0 , s , wordDict ) ;
-        return ans ;  
+        n = s.size() , m = wordDict.size() ;
+        return check( 0 , s , wordDict ) ;
     }
 };
-
-
-
-
-
-
-
 
 
 
