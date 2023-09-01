f83a569b49999a324689e89ac06c34a04fd0a003
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-09-01 10:39:41 +0530

    Time: 3 ms (89.82%), Space: 8.5 MB (14.67%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,144 @@
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+// /// // /// /// /////    try  to  solve   follow - up  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int> ans ; 
+        for(int i=0; i<=n; i++)
+        {
+            ans.push_back( __builtin_popcount(i)  ) ;
+        }
+        return ans ;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> countBits(int n) 
+//     {
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
