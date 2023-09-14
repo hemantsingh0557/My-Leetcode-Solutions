dd451198b5ad4ac20b2368017ed59b66c27a1c43
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-09-14 09:58:40 +0530

    Time: 15 ms (89.66%), Space: 15.3 MB (36.34%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -45,14 +45,22 @@
 
 
 
+
+
+
+
+
 // //
-// // // // // // //  Solve Using  Only One  Method  ===>     
-// // // // // // //  Solve Using  Only One  Method  ===>                        
+// // // // // // //  Solve Using  Two  Method  ===>     
+// // // // // // //  Solve Using  Two  Method  ===>                        
 // //                     
 // //                     
 // //           // here n is total map size of key and e is total key's values
 // //                     
 // //  1st Method ==> DFS(Recursion) and  Euler Path  TC = O( n^2 * log(n) ) , SC = O(n+e) 
+// //
+// //
+// //  2nd Method ==> DFS(Recursion) and  Euler Path  TC = O( n^2 * log(n) ) , SC = O(n+e) 
 // //                      
 // //                      
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -82,36 +90,107 @@ class Solution {
 public:
     unordered_map<string , vector<string> > mp  ;  
     vector<string> ans ; 
-    void dfs( string airport )
+    int n ; 
+    bool dfs( string airport , vector<string> &path )
     {
-        vector<string> &Airports = mp[airport] ;
-        while( Airports.size() )
+        path.push_back( airport ) ;
+        if( path.size() == n+1)
         {
-            string nextAirport = Airports.back() ;
-            Airports.pop_back() ;
-            dfs(nextAirport) ;
+            ans = path ;
+            return true ;
         }
-        ans.push_back(airport) ; 
+        vector<string> &neighbours = mp[airport] ;
+        int sz = neighbours.size() ;
+        for(int i=0; i<sz; i++)
+        {
+            string nextAirport = neighbours[i] ;
+            neighbours.erase(neighbours.begin() + i) ;
+            if( dfs(nextAirport , path ) )  return true ;;
+            neighbours.insert(neighbours.begin()+i , nextAirport ) ;
+        }
+        path.pop_back() ; 
+        return false ; 
     }
     // // // // //    Main Function of Question    ==========>
     // // // // //    Main Function of Question    ==========>
     //
     vector<string> findItinerary(vector<vector<string>>& tickets) 
     {
-        int n = tickets.size() ;
+        n = tickets.size() ;
         for(int i=0; i<n; i++)
         {
             mp[tickets[i][0]].push_back( tickets[i][1] ) ;   
         }  
         for(auto &it : mp)
         {
-            sort( it.second.rbegin() , it.second.rend() ) ;   
+            sort( it.second.begin() , it.second.end() ) ;   
         }
-        dfs( "JFK" ) ;
-        reverse( ans.begin() , ans.end() ) ;  
+        vector<string>path ; 
+        bool find  = dfs( "JFK" , path ) ;
         return ans ;  
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  2nd  Method    ======>    DFS(Recursion) and  Euler Path 
+// // //  2nd  Method    ======>    DFS(Recursion) and  Euler Path 
+// // //  2nd  Method    ======>    DFS(Recursion) and  Euler Path 
+// //                             
+// //                             
+// //        // here n is total map size of key and e is total key's values                        
+// //                             
+// // //   Time Complexity   = O( n^2 * log(n) )   
+// //                              
+// // //   Space Complexity  =  O(n+e) 
+// //                               
+
+
+// class Solution {
+// public:
+//     unordered_map<string , vector<string> > mp  ;  
+//     vector<string> ans ; 
+//     void dfs( string airport )
+//     {
+//         vector<string> &Airports = mp[airport] ;
+//         while( Airports.size() )
+//         {
+//             string nextAirport = Airports.back() ;
+//             Airports.pop_back() ;
+//             dfs(nextAirport) ;
+//         }
+//         ans.push_back(airport) ; 
+//     }
+//     // // // // //    Main Function of Question    ==========>
+//     // // // // //    Main Function of Question    ==========>
+//     //
+//     vector<string> findItinerary(vector<vector<string>>& tickets) 
+//     {
+//         int n = tickets.size() ;
+//         for(int i=0; i<n; i++)
+//         {
+//             mp[tickets[i][0]].push_back( tickets[i][1] ) ;   
+//         }  
+//         for(auto &it : mp)
+//         {
+//             sort( it.second.rbegin() , it.second.rend() ) ;   
+//         }
+//         dfs( "JFK" ) ;
+//         reverse( ans.begin() , ans.end() ) ;  
+//         return ans ;  
+//     }
+// };
 
 
 
