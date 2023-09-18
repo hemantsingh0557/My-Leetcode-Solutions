778ceb23e33e6e953b66e80f470d91d24c45ffb8
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-09-18 09:36:48 +0530

    Time: 10 ms (54.38%), Space: 10.8 MB (13.97%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -71,23 +71,23 @@
 
 
 
-class Solution {
-public:
-    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
-    {
-        int m = mat.size()  ,   n = mat[0].size() ; 
-        vector<pair<int,int> > allSum ;
-        for(int i=0; i<m; i++)
-        {
-            int sum = accumulate( mat[i].begin() , mat[i].end() , 0 ) ;
-            allSum.push_back({sum, i}) ;  
-        }
-        sort( allSum.begin() , allSum.end()  )  ;
-        vector<int> ans ;    
-        for(int i=0; i<k; i++) ans.push_back( allSum[i].second ) ;  
-        return ans ;
-    }
-};
+// class Solution {
+// public:
+//     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
+//     {
+//         int m = mat.size()  ,   n = mat[0].size() ; 
+//         vector<pair<int,int> > allSum ;
+//         for(int i=0; i<m; i++)
+//         {
+//             int sum = accumulate( mat[i].begin() , mat[i].end() , 0 ) ;
+//             allSum.push_back({sum, i}) ;  
+//         }
+//         sort( allSum.begin() , allSum.end()  )  ;
+//         vector<int> ans ;    
+//         for(int i=0; i<k; i++) ans.push_back( allSum[i].second ) ;  
+//         return ans ;
+//     }
+// };
 
 
 
@@ -110,22 +110,22 @@ public:
 
 
 
-// class Solution {
-// public:
-//     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
-//     {
-//         int m = mat.size()  ,   n = mat[0].size() ; 
-//         multimap<int,int> mp  ;
-//         for(int i=0; i<m; i++)
-//         {
-//             int sum = accumulate( mat[i].begin() , mat[i].end() , 0 ) ;
-//             mp.insert( {sum , i} ) ;   
-//         }
-//         vector<int> ans ;    
-//         for(auto & it : mp ) if( k-- > 0 ) ans.push_back( it.second )   ;  
-//         return ans ;
-//     }
-// };
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
+    {
+        int m = mat.size()  ,   n = mat[0].size() ; 
+        multimap<int,int> mp  ;
+        for(int i=0; i<m; i++)
+        {
+            int sum = accumulate( mat[i].begin() , mat[i].end() , 0 ) ;
+            mp.insert( {sum , i} ) ;   
+        }
+        vector<int> ans ;    
+        for(auto & it : mp ) if( k-- > 0 ) ans.push_back( it.second )   ;  
+        return ans ;
+    }
+};
 
 
 
