51bc5103411fae66ca762c0be58727f322f3df57
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2023-09-30 21:59:24 +0530

    Time: 120 ms (33.33%), Space: 85.1 MB (16.67%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,201 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums) 
+//     {
+//          
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// // // // // // //  Solve Using  Only One  Method  ===>     
+// // // // // // //  Solve Using  Only One  Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method ( Two Ways )  ====>  Simple Using map   TC = O(n) , SC = O(n) 
+// //                      
+// //                     
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method ( Two Ways )   ======>     Simple Using map  
+// // //  1st Method ( Two Ways )   ======>     Simple Using map  
+// // //  1st Method ( Two Ways )   ======>     Simple Using map  
+// //                             
+// // //   Time Complexity   = O(n)   
+// //                              
+// // //   Space Complexity  = O(n)   
+// //                               
+
+
+
+
+
+// // //   1st  Method   ====>      1st way   =====>       
+// // //   1st  Method   ====>      1st way   =====>       
+// // //   1st  Method   ====>      1st way   =====>       
+
+
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums) 
+//     {
+//         int n = nums.size() ;  
+//         map<int , int> mp; 
+//         for(int i=0; i<n; i++)
+//         {
+//             mp[nums[i]] ++ ; 
+//         }
+//         int ans = 0  ;  
+//         for(auto it : mp)
+//         {
+//             if( it.second == 1 ) return -1 ; 
+//             else if( it.second % 3 == 0  ) ans += it.second / 3 ; 
+//             else if( it.second % 2 == 0  )
+//             {
+//                 int times = it.second / 3 ;  
+//                 int ct = it.second % 3   ;    
+//                 if( ct == 1 )  ans += times + 1 ;  //  ans += times - 1 + 2   ;  
+//                 else ans += times + 1 ; 
+//             }
+//             else
+//             {
+//                 int times = it.second / 3 ;  
+//                 int ct = it.second % 3   ;    
+//                 if( ct == 1 )  ans += times + 1 ;
+//                 else ans += times + 1 ; 
+//             }
+//         }
+//         return ans ; 
+//     }
+// };
+
+
+
+
+
+
+
+// // //   1st  Method   ====>     2nd  way   =====>       
+// // //   1st  Method   ====>     2nd  way   =====>       
+// // //   1st  Method   ====>     2nd  way   =====>       
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) 
+    {
+        int n = nums.size() ;  
+        unordered_map<int , int> mp; 
+        for(int i=0; i<n; i++)
+        {
+            mp[nums[i]] ++ ; 
+        }
+        int ans = 0  ;  
+        for(auto it : mp)
+        {
+            if( it.second == 1 ) return -1 ; 
+            int times = it.second / 3 ;  
+            int rem = it.second % 3   ;  
+            ans += times ;   
+            if( rem > 0 )  ans ++ ;
+        }
+        return ans ; 
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
