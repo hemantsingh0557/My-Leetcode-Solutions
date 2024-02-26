8843fedc7bd2043a9aab25935cf31503f7cb1f88
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2024-02-26 10:50:55 +0530

    Time: 2 ms (45.91%), Space: 11.6 MB (25.40%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,118 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool isSameTree(TreeNode* p, TreeNode* q) 
+//     {
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// // // // // // //  Solve Using  Only One  Method  ===>     
+// // // // // // //  Solve Using  Only One  Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method  ====> Simple Recursion    TC = O(n) , SC = O(1) constant
+// //                      
+// //                      
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method    ======>   Simple Recursion
+// // //  1st Method    ======>   Simple Recursion
+// // //  1st Method    ======>   Simple Recursion
+// //                             
+// // //   Time Complexity   = O(n)  //  n is no of nodes  
+// //                              
+// // //   Space Complexity  = O(1) constant  
+// //                               
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if( !p && !q ) return true ; 
+        if( !p || !q ) return false ; 
+        if( p->val != q->val ) return false ; 
+        if( ! isSameTree( p->left , q->left ) ) return false ;
+        if( ! isSameTree( p->right , q->right ) ) return false ;
+        return true ; 
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
