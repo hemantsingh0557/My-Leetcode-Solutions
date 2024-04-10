c7786a1733b2db6d643c4136b655aa31abf97a5d
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2024-04-10 20:12:12 +0530

    Time: 5 ms (32.04%), Space: 10.8 MB (91.52%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,204 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// // // // // // //  Solve Using  Three   Method  ===>     
+// // // // // // //  Solve Using  Three   Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method  ====> Using Queue Simulation    TC = O(n*log(n)) , SC = O(n) 
+// //                      
+// //                      
+// // //  2nd Method  ====> Using Two Pointer    TC = O(n*log(n)) , SC = O(log(n)) 
+// //                     
+// //                     
+// // //  3rd Method  ====>  Try any other  TC = O() , SC = O()  
+// //                     
+// //                     
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method    ======>    Using Queue Simulation   
+// // //  1st Method    ======>    Using Queue Simulation  
+// // //  1st Method    ======>    Using Queue Simulation  
+// //                             
+// // //   Time Complexity   = O(n*log(n))
+// //                              
+// // //   Space Complexity  = O(n)   
+// //                               
+
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) 
+//     {
+//         int n = deck.size() ; 
+//         queue<int> q ; 
+//         for(int i=0; i<n; i++) q.push(i) ;  
+//         sort(deck.begin() , deck.end() ) ;
+//         vector<int> ans(n)  ;
+//         for(int i=0; i<n; i++)
+//         {
+//             ans[q.front()] = deck[i] ; 
+//             q.pop() ; 
+//             if( q.size() )
+//             {
+//                 q.push(q.front()) ; 
+//                 q.pop() ; 
+//             }
+//         }
+//         return ans  ; 
+//     }
+// };
+
+
+
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  2nd Method    ======>   Using Two Pointer 
+// // //  2nd Method    ======>   Using Two Pointer 
+// // //  2nd Method    ======>   Using Two Pointer 
+// //                             
+// // //   Time Complexity   = O(n*log(n))   
+// //                              
+// // //   Space Complexity  = O(log(n))   
+// //                               
+
+
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) 
+    {
+        int n = deck.size() ; 
+        sort(deck.begin() , deck.end() ) ;
+        vector<int> ans(n,0)  ;
+        int deckInd = 0 , ansInd = 0 ,  skip = 0 ; 
+        while(deckInd<n)
+        {
+            if( ans[ansInd] == 0 )
+            {
+                if( ! skip )
+                {
+                    ans[ansInd] = deck[deckInd] ;
+                    deckInd ++ ; 
+                }
+                skip = !skip ; 
+            }
+            ansInd = (ansInd+1) % n ; 
+        }
+        return ans  ; 
+    }
+};
+
+ 
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  3rd Method    ======>   Try any other
+// // //  3rd Method    ======>   Try any other
+// // //  3rd Method    ======>   Try any other
+// //                             
+// // //   Time Complexity   = O()   
+// //                              
+// // //   Space Complexity  = O()   
+// //                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
