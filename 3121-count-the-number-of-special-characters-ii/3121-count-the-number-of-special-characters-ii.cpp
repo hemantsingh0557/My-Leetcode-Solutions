3817a0b14d32dff2fa73b2b32f67cc78c54af058
Hemant Singh <111867265+hemantsingh0557@users.noreply.github.com>
2024-04-21 09:57:22 +0530

    Time: 72 ms (85.71%), Space: 23.2 MB (71.43%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,207 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// //  LeetCode  Weekly Contest  394  ( Able To Solve Three Question(third takes more time in implement) )
+
+
+// //  2nd  Question  ===> Able to solve in contest
+
+
+// //  Today's  Date   -    21st April 2024  -  Sunday    .    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int numberOfSpecialChars(string word) 
+//     {
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// // // // // // //  Solve Using  Two   Method  ===>     
+// // // // // // //  Solve Using  Two   Method  ===>                        
+// //                     
+// //                     
+// // //  1st Method  ====> Using hashing    TC = O(n+26) , SC = O(26) 
+// //                      
+// //                      
+// // //  2nd Method  ====> Try any other    TC = O() , SC = O() 
+// //                     
+// //                     
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// //                           
+// //                           
+// // //  1st Method    ======>  Using hashing 
+// // //  1st Method    ======>  Using hashing 
+// // //  1st Method    ======>  Using hashing 
+// //                             
+// // //   Time Complexity   = O(n+26)   
+// //                              
+// // //   Space Complexity  = O(26)   
+// //                               
+
+
+
+class Solution {
+public:
+    int numberOfSpecialChars(string word) 
+    {
+        int n = word.size() ;
+        int ans = 0 ;
+        vector<int> up(26, INT_MAX), lo(26 , -1) ;
+        for(int i=0; i<n ; i++)
+        {
+            if( word[i] >= 97 ) lo[word[i]-'a'] = i ; 
+            else
+            {
+                up[word[i]-'A'] = min( up[word[i]-'A'] , i ) ; 
+            }
+        }
+        for(int i=0; i<26; i++)
+        {
+            if( lo[i] == -1 || up[i] == INT_MAX ) continue ;
+            else if( lo[i] < up[i] ) ans ++ ; 
+        }
+        return ans ; 
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //                           
+// //                           
+// // //  2nd Method    ======>  Try any other    
+// // //  2nd Method    ======>  Try any other    
+// // //  2nd Method    ======>  Try any other    
+// //                             
+// // //   Time Complexity   = O()   
+// //                              
+// // //   Space Complexity  = O()   
+// //                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
